60363aa2190213c810985ff7ba7c8b7800683f83
Yuancheng Zhang <old.far.hot@gmail.com>
2019-06-04 14:43:15 +0800

    a mini demo

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(50451, () => {
+    console.info('Running on port 50451');
+});
+
+// Routes
+app.use('/api/discord', require('./api/discord'));
+
+
+app.use((err, req, res, next) => {
+    switch (err.message) {
+        case 'NoCodeProvided':
+            return res.status(400).send({
+                status: 'ERROR',
+                error: err.message,
+            });
+        default:
+            return res.status(500).send({
+                status: 'ERROR',
+                error: err.message,
+            });
+    }
+});
+
+//use sessions for tracking logins
+app.use(session({
+    secret: 'bulb market halo',
+    resave: true,
+    saveUninitialized: false
+  }));
